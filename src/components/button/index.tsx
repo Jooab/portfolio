aa2ecbefa4561d567521777cf038f133d40b7680
joab-prates <joobascanal@gmail.com>
2025-02-21 20:51:05 -0300

    adicionado curriculo para download no botao

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -3,7 +3,7 @@ import { FaCloudDownloadAlt } from "react-icons/fa";
 
 export const Button = () => {
   return (
-    <StyledButton>
+    <StyledButton as="a" href="/images/Currículo Dev-Joab Prates.pdf" download="Currículo Dev-Joab Prates">
       Currículo <FaCloudDownloadAlt />
     </StyledButton>
   );
