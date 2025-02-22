0b1833aeb41d6b515037f0863bc81d22b57b0935
joab-prates <joobascanal@gmail.com>
2025-02-21 21:07:17 -0300

    atualizado o pdf do currículo

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -3,7 +3,7 @@ import { FaCloudDownloadAlt } from "react-icons/fa";
 
 export const Button = () => {
   return (
-    <StyledButton as="a" href="/images/Currículo Dev-Joab Prates.pdf" download="Currículo Dev-Joab Prates">
+    <StyledButton as="a" href="/images/Currículo Dev - Joab Prates.pdf" download="Currículo Dev - Joab Prates">
       Currículo <FaCloudDownloadAlt />
     </StyledButton>
   );
